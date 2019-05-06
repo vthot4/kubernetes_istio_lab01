0c68034fd3a64e74aca56efbd7e7f918a0fd62b0
root <root@PROTON.localdomain>
2019-05-07 00:52:12 +0200

    add gke_cluster script

--- gke_cluster.sh
+++ gke_cluster.sh
@@ -18,24 +18,46 @@ readonly USER='admin'
 readonly GKE_VERSION='1.11.8-gke.6'
 readonly MACHINE_TYPE='n1-standard-1'
 readonly IMAGE_TYPE='COS'
-readonly NUM_NODES='3'
+readonly NUM_NODES='1'
 
-gcloud beta container \
-    --project ${PROJECT_ID} clusters create ${CLUSTER_NAME} \ 
-    --zone ${ZONE} \
-    --username ${USER} \
-    --cluster-version  ${GKE_VERSION} \
-    --machine-type ${MACHINE_TYPE} \
-    --image-type ${IMAGE_TYPE} \
-    --disk-type "pd-standard" \
-    --disk-size "100" \
-    --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
-    --num-nodes ${NUM_NODES} \
-    --enable-cloud-logging \
-    --enable-cloud-monitoring \ 
-    --enable-ip-alias \ 
-    --network "projects/${PROJECT_ID}/global/networks/default" \
-    --subnetwork "projects/${PROJECT_ID}/regions/${ZONE}/subnetworks/default" \
-    --addons HorizontalPodAutoscaling,HttpLoadBalancing,KubernetesDashboard \
-    --enable-autoupgrade \
-    --enable-autorepair 
+
+help()
+{
+	echo ""
+	echo $0:
+	echo "	create	Create a cluster gke with the specifications in CONSTANTS."
+	echo "	delete	Delete gke cluster"
+	echo "	help	show help"
+	echo ""		
+}
+
+case $1 in
+create)
+	gcloud beta container \
+	--project ${PROJECT_ID} clusters create ${CLUSTER_NAME} \
+	--zone ${ZONE} \
+	--username ${USER} \
+	--cluster-version ${GKE_VERSION} \
+	--machine-type ${MACHINE_TYPE} \
+	--image-type "COS" \
+	--disk-type "pd-standard" \
+	--disk-size "100" \
+	--scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
+	--num-nodes ${NUM_NODES} \
+	--enable-cloud-logging \
+	--enable-cloud-monitoring \
+	--no-enable-ip-alias \
+	--network "projects/hackio/global/networks/default" \
+	--subnetwork "projects/hackio/regions/europe-west3/subnetworks/default" \
+	--addons HorizontalPodAutoscaling,HttpLoadBalancing,KubernetesDashboard \
+	--enable-autoupgrade \
+	--enable-autorepair
+	;;
+delete)
+	gcloud container clusters delete lab01 --zone "europe-west3-c"
+	;;
+*)
+	help
+	;;
+esac
+
